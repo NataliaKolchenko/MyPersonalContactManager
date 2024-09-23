9758b6331181108fa8ccdbd29dba143dae0dcce8
Natalia <kolchenko.nata.yu@gmail.com>
2024-09-23 11:46:35 +0200

    added new configuration file WebConfig

---
+++ src/main/java/com/example/MyPersonalContactManager/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.example.MyPersonalContactManager.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final AuthenticationInterceptor authenticationInterceptor;
+
+    public WebConfig(AuthenticationInterceptor authenticationInterceptor) {
+        this.authenticationInterceptor = authenticationInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationInterceptor)
+                .addPathPatterns("/MyPersonalContactManager/**");
+    }
+}
