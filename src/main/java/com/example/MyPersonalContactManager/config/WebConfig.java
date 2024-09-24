3b2515055e4eed08ce24170722f2f8b775991c90
Natalia <kolchenko.nata.yu@gmail.com>
2024-09-24 14:48:59 +0200

    подружила Контакты с JWT. сейчас метод getAllContact отдает 200 (без данных)

--- src/main/java/com/example/MyPersonalContactManager/config/WebConfig.java
+++ src/main/java/com/example/MyPersonalContactManager/config/WebConfig.java
@@ -1,21 +1,59 @@
 package com.example.MyPersonalContactManager.config;
 
-import com.example.MyPersonalContactManager.infrastructure.AuthenticationInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-    private final AuthenticationInterceptor authenticationInterceptor;
+@EnableWebSecurity
+//public class WebConfig implements WebMvcConfigurer {
+//    private final AuthenticationInterceptor authenticationInterceptor;
+//
+//    public WebConfig(AuthenticationInterceptor authenticationInterceptor) {
+//        this.authenticationInterceptor = authenticationInterceptor;
+//    }
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(authenticationInterceptor)
+//                .addPathPatterns("/MyPersonalContactManager/**");
+//    }
+//}
 
-    public WebConfig(AuthenticationInterceptor authenticationInterceptor) {
-        this.authenticationInterceptor = authenticationInterceptor;
+public class WebConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // Отключение CSRF для упрощения (в продакшене нужно включить)
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/contacts").authenticated() // Требует аутентификации для /contacts
+                        .anyRequest().permitAll() // Остальные запросы разрешены для всех
+                );
+//                .httpBasic(); // Использование Basic Authentication
+
+        return http.build();
     }
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(authenticationInterceptor)
-                .addPathPatterns("/MyPersonalContactManager/**");
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.inMemoryAuthentication()
+                .withUser("user").password(passwordEncoder().encode("password")).roles("USER");
+        return authenticationManagerBuilder.build();
     }
 }
--- src/main/java/com/example/MyPersonalContactManager/controller/ContactController.java
+++ src/main/java/com/example/MyPersonalContactManager/controller/ContactController.java
@@ -5,13 +5,20 @@ import com.example.MyPersonalContactManager.models.ContactModels.ContactDTOBig;
 import com.example.MyPersonalContactManager.models.Error;
 import com.example.MyPersonalContactManager.models.RequestResponseModels.RequestBodyClient;
 import com.example.MyPersonalContactManager.models.RequestResponseModels.ResponseAPI;
+import com.example.MyPersonalContactManager.models.UserModels.User;
 import com.example.MyPersonalContactManager.service.ContactServiceImp;
 import com.example.MyPersonalContactManager.service.UserServiceImp;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
 import java.util.List;
 
 @RestController
@@ -61,25 +68,45 @@ public class ContactController {
     }
 
     @GetMapping(value = "/contacts")
-    public ResponseEntity<ResponseAPI> getAllContacts(@RequestHeader("token") String token) {
+    public ResponseEntity<ResponseAPI> getAllContacts(HttpServletRequest request) {
         responseAPI = new ResponseAPI();
-        if (token == null || token.isEmpty()) {
-            responseAPI.response = new Error(400, "Authorization header is missing.");
-            return ResponseEntity.badRequest().body(responseAPI);
-        }
+        // Извлекаем информацию о текущем пользователе // Получаем текущую аутентификацию
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        boolean userRole = dbUserService.getUserRoleByToken(token);
-        String userId = dbUserService.getUserIdByToken(token);
+        // Извлечение ролей (userRoles)
+        Collection<? extends GrantedAuthority> roles = authentication.getAuthorities();
+        List<String> userRoles = roles.stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
 
-        if (userId.isEmpty()) {
-            responseAPI.response = new Error(403, "Access denied.");
-        } else if (userRole) {
-            List<Contact> allContacts = dbContactServiceImp.getAllContacts();
-            responseAPI.response = allContacts;
+        System.out.println(userRoles);
+
+        // Извлекаем информацию о пользователе
+        Object principal = authentication.getPrincipal();
+        String userId = null;
+        if (principal instanceof UserDetails) {
+            // Пример получения userId, если он хранится в UserDetails
+            userId = ((User) principal).getUserId();
         } else {
-            List<Contact> contactListByUserId = dbContactServiceImp.getContactByUserId(userId);
-            responseAPI.response = contactListByUserId;
+            // Если principal — это просто имя пользователя (например, если не используется UserDetails)
+            userId = principal.toString();
         }
+        System.out.println(userId);
+
+        List<Contact> allContacts = dbContactServiceImp.getAllContacts();
+
+//        boolean userRole = dbUserService.getUserRoleByToken(token);
+//        String userId = dbUserService.getUserIdByToken(token);
+
+//        if (userId.isEmpty()) {
+//            responseAPI.response = new Error(403, "Access denied.");
+//        } else if (userRole) {
+//            List<Contact> allContacts = dbContactServiceImp.getAllContacts();
+//            responseAPI.response = allContacts;
+//        } else {
+//            List<Contact> contactListByUserId = dbContactServiceImp.getContactByUserId(userId);
+//            responseAPI.response = contactListByUserId;
+//        }
 
         return ResponseEntity.ok(responseAPI);
     }
